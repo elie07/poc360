30f5d23f2a2f06d6fa8dd8ef367560246136cc3c
Elie Yelfouf <elie.yelfouf@sewan.fr>
2020-08-05 15:34:40 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PannellumReact from './PannellumReact';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='cont'>
+          <h1>Poc 360 image</h1>
+          <PannellumReact/>
+        </div>
       </header>
     </div>
   );
